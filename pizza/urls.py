261aa99993b5ce2b258226836b217d6710ace4ee
majaharkot <harkot.majka@gmail.com>
2019-11-07 08:46:09 +0100

    ulepszony szablon

--- pizza/urls.py
+++ pizza/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'pizza'
+
 urlpatterns = [
 	path('', views.index, name='index'),
     path('komunikat/', views.komunikat, name='komunikat'),
